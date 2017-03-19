0cde1bf9edde7f390ed07e93f78ea1f96f7ddfe2
Braun Patrik <braun.patrik@aut.bme.hu>
2017-03-19 22:42:28 +0100

    udpate

---
+++ MobSoftLab/app/src/main/java/hu/bme/aut/mobsoft/mobsoftlab/ui/main/MainActivity.java
@@ -0,0 +1,15 @@
+package hu.bme.aut.mobsoft.mobsoftlab.ui.main;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import hu.bme.aut.mobsoft.mobsoftlab.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ MobSoftLab/app/src/main/java/hu/bme/aut/mobsoft/mobsoftlab/ui/main/MainScreen.java
@@ -0,0 +1,8 @@
+package hu.bme.aut.mobsoft.mobsoftlab.ui.main;
+
+/**
+ * Created by Patrik on 2017. 03. 19..
+ */
+
+public class MainScreen {
+}
